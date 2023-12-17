8308ca14ccfc3f46cde196fa393059e15d343c82
heyitsjearbear <jereberries1215@gmail.com>
2023-12-16 22:02:49 -0800

    Add react-router-dom package and update App component to support routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import Logo from './assets/logo.png'
-import heroPic from './assets/hero.png'
-import Hero from './components/Hero'
-import NavigationBar from './components/NavigationBar'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+// Import other pages as needed
+
 const App = () => {
-  const [isNavVisible, setIsNavVisible] = useState(false);
-  const toggleNav = () => {
-    setIsNavVisible(!isNavVisible);
-    console.log(isNavVisible);
-  }
-  const navItems = [
-    {name: 'Home'},
-    {name: 'About'},
-    {name: 'Contact'},
-    {name: 'Portfolio'},
-    {name: 'QnA'},
-    {name: 'Socials'},
-    {name: 'Contact'}
-  ]
   return (
-    <>
-      <Header logoPath = {Logo} toggleNav={toggleNav} />
-      <Hero title = 'Krisell Bakes' heroPic = {heroPic} />
-      <NavigationBar items = {navItems} toggleNav={toggleNav} isNavVisible={isNavVisible}/>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
