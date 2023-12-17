f781e9fdba40c27875e03c8a770cee19fa63c76e
heyitsjearbear <jereberries1215@gmail.com>
2023-12-17 10:54:59 -0800

    Add header and navigation bar to app component so that header and hav bar are accessible across all routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
+import Header from "./components/Header";
+import NavigationBar from "./components/NavigationBar";
 // Import other pages as needed
-
+import Logo from "./assets/logo.png";
+import { useState } from "react";
 const App = () => {
+  const [isNavVisible, setIsNavVisible] = useState(false);
+  const toggleNav = () => {
+    setIsNavVisible(!isNavVisible);
+  };
+  const navItems = [
+    { name: "Home", route: "/" },
+    { name: "About", route: "/about" },
+    // Add more items as needed
+  ];
+
   return (
     <Router>
+      <Header logoPath={Logo} toggleNav={toggleNav} />
+      <NavigationBar
+        items={navItems}
+        toggleNav={toggleNav}
+        isNavVisible={isNavVisible}
+      />
       <Routes>
         <Route path="/" element={<HomePage />} />
         {/* Add more routes as needed */}
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,37 +1,13 @@
-import { useState } from "react";
-import Logo from "../assets/logo.png";
 import heroPic from "../assets/hero.png";
-import Header from "./Header";
-import NavigationBar from "./NavigationBar";
 import Hero from "./Hero";
 import '../styles/HomePage.css';
 
 const HomePage = () => {
-  const [isNavVisible, setIsNavVisible] = useState(false);
-  const toggleNav = () => {
-    setIsNavVisible(!isNavVisible);
-    console.log(isNavVisible);
-  };
-  const navItems = [
-    { name: "Home", route: "/" },
-    { name: "About", route: "/about" },
-    { name: "Contact", route: "/contact" },
-    { name: "Portfolio", route: "/portfolio" },
-    { name: "QnA", route: "/qna" },
-    { name: "Socials", route: "/socials" },
-    { name: "Contact", route: "/contact" },
-  ];
   return (
     <>
-      <Header logoPath={Logo} toggleNav={toggleNav} />
       <div className="hero-container">
         <Hero title="Krisell Bakes" heroPic={heroPic} />
       </div>
-      <NavigationBar
-        items={navItems}
-        toggleNav={toggleNav}
-        isNavVisible={isNavVisible}
-      />
     </>
   );
 };
